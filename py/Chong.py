a196214158db51dc4dbe5fdf6b3bed612515ddf6
Malloc_Luo <2423049596@qq.com>
2020-03-31 23:25:08 +0800

    addHJY

--- py/Chong.py
+++ py/Chong.py
@@ -6,16 +6,25 @@ Created on Sat Sep 21 12:20:13 2019
 """
 
 import random as rd
-import requests as rq
-#import os
+
+import os
+#import sys
 from functools import reduce
+
+try:
+	import requests as rq
+except ImportError:
+	os.system('pip install requests')
+	import requests as rq
+	
 
 #空格用来占位，不要删，若添加成员直接加在后面
 Name = ['曹澎骏', '黄竟洋', '李昱棋', '彭阳', '钟毅', '冯雨霖', '徐俊杰', '赵东阳', ' ']
 
 #希望收到推送，请前往server酱注册，添加自己的sckey
 MyUrl = ["SCU90245T00180f90cbc0e776a748f7bd2b0d549a5e735cee58ca4",
-        "SCU89179Tf841e000610475dc6c0e6e16605ae1205e6afd006336e"]
+        "SCU89179Tf841e000610475dc6c0e6e16605ae1205e6afd006336e",
+		"SCU89175Tb0d12fcc250b7e3dfe9ebe29fd20acb05e6b786f493a3"]
 
 title = '土豆条水分专用'
 msg = ''
@@ -24,9 +33,12 @@ group = '土豆条电控水群'
 
 #os.system('qqbot')
 
+#def Rule(info = '\n分组方式（例如：332）\n\n\t: '):
+#	return list(map(int, list(input(info))))
+
 def SelfRule(rule):
 	while reduce(lambda a, b: a + b, rule) != len(Name):
-		rule = list(map(int, list(input('\n分组人数不对，请重新输~\n\n\t: '))))	
+		rule = list(map(int, list(input('\n分组人数不对，请重新输~\n\n\t: '))))		
 	for r in rule:
 		p = []
 		while len(p) < r:
@@ -36,17 +48,16 @@ def SelfRule(rule):
 		print(p)
 		result.append(p)
 	
-
 def d(name):
 	Name.remove(name)
 	
 def a(name):
 	Name.append(name)
-
-print('少人吗？[ y/ n/ 少/ 不少/]')
+	
+print('少人吗？[ y/ n/ 少/ 不少/ 多/]')
 isLack = input('\n\t: ')
 
-while isLack in ['y', '少', 'shao', 'sao', '烧', '骚', 'Y', '有', '一']:
+while isLack in ['y', '少', 'shao', 'sao', '烧', '骚', 'Y', '有', '一', '多', 'duo']:
 	try:
 		eval(input('\n用 d(name) 临时删人，如 d(\'谦总\'); 用 a(name) 临时加人，如 a(\'谦总\'); 若选错了使用 d(\' \')\n\n\t: '))
 		if ' ' not in Name:
@@ -71,6 +82,6 @@ for u in MyUrl:
 
 #os.system('qq send group ' + group + ' ' + info)
      
-input('continue')        
+input('\n\n已推送...')        
 
     
